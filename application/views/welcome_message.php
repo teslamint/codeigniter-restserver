0264272c718151310e4a9528e2fe4239a3c11b2d
Alan Garcia <agarcia@kbsp.com>
2012-01-18 03:42:24 +0800

    Integreated jquery jquery mobile and user agenet detection for use of either a default or mobile template

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,53 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-
-<style type="text/css">
-
-body {
- background-color: #fff;
- margin: 40px;
- font-family: Lucida Grande, Verdana, Sans-serif;
- font-size: 14px;
- color: #4F5155;
-}
-
-a {
- color: #003399;
- background-color: transparent;
- font-weight: normal;
-}
-
-h1 {
- color: #444;
- background-color: transparent;
- border-bottom: 1px solid #D0D0D0;
- font-size: 16px;
- font-weight: bold;
- margin: 24px 0 2px 0;
- padding: 5px 0 6px 0;
-}
-
-code {
- font-family: Monaco, Verdana, Sans-serif;
- font-size: 12px;
- background-color: #f9f9f9;
- border: 1px solid #D0D0D0;
- color: #002166;
- display: block;
- margin: 14px 0 14px 0;
- padding: 12px 10px 12px 10px;
-}
-
-</style>
-</head>
-<body>
-
-<h1>Welcome to CodeIgniter!</h1>
-
-<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
 <ul>
 	<li><a href="<?php echo site_url('api/example/users');?>">Users</a> - defaulting to XML</li>
@@ -57,11 +7,8 @@ code {
 	<li><a id="ajax" href="<?php echo site_url('api/example/users/format/json');?>">Users</a> - get it in JSON (AJAX request)</li>
 </ul>
 
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
 <p><br />Page rendered in {elapsed_time} seconds</p>
 
-<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 $(function(){
 	// Bind a click event to the 'ajax' object id
@@ -85,5 +32,3 @@ $(function(){
 });
 </script>
 
-</body>
-</html>
