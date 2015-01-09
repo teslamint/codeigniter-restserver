ddd26791537d0e8745951ea609861b642d4a1791
Jaehoon You <teslamint@gmail.com>
2015-01-09 17:38:32 +0900

    Revert "Integreated jquery jquery mobile and user agenet detection for use of either a default or mobile template"
    This reverts commit 0264272c718151310e4a9528e2fe4239a3c11b2d.

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -5,42 +5,12 @@ class Welcome extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
-		$this->load->library('user_agent');
-		$this->load->library('mobile');
-		$this->load->helper('url');
 	}
 
 	function index()
 	{
-    		if($this->agent->is_mobile()){
-
-
-			$this->mobile->header('Welcome to CodeIgniter!', 'e')->button('welcome/help', 'Help', 'info');
-
-			$this->mobile->navbar(array(
-				'welcome/index' 	=> array('text' => 'Home', 		'icon' => 'home'),
-				'welcome/settings'	=> array('text' => 'Settings', 	'icon' => 'gear')
-			), 'a');
-
-			$this->mobile->footer('Footer', 'a');
-
-			$this->mobile->view('welcome_message');
-		}else{
-
-			$this->template->header('Welcome to CodeIgniter!', 'e')->button('welcome/help', 'Help', 'info');
-
-			$this->template->navbar(array(
-				'welcome/index' 	=> array('text' => 'Home', 		'icon' => 'home'),
-				'welcome/settings'	=> array('text' => 'Settings', 	'icon' => 'gear')
-			), 'a');
-
-			$this->template->footer('Footer', 'a');
-
-
-			$this->template->view('welcome_message');
-			
-		}
-
+		$this->load->helper('url');
+		$this->load->view('welcome_message');
 	}
 }
 
